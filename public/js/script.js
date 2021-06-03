cc43323f337f7dcbdc133a5c5465fbdf2af46764
Ibrahima Timera <imtimera01@gmail.com>
2021-06-03 11:41:16 +0200

    Rotation and remove OK

--- public/js/script.js
+++ public/js/script.js
@@ -1,65 +1,104 @@
+import { randomColor } from './utils.js';
+
 function initDraw() {
     var canvas = document.getElementById('shape-area')
-    var startDraw = false
+    var drawing = false
+    var rotation = false
+    var tabItemToRotate = []
+    var tabItemToRemove = []
+    var index = 0
+    const shapeArea = 'shape-area'
+    const rectangle = 'rectangle'
+    const px = 'px'
+
     var shape = {
         x: 0,
         y: 0,
         startX: 0,
         startY: 0
     }
-
     function setMousePosition(e) {
         var ev = e || window.event
         if (ev.pageX) {
             shape.x = ev.pageX + window.pageXOffset
             shape.y = ev.pageY + window.pageYOffset
         } else if (ev.clientX) {
-            shape.x = ev.clientX + document.body.scrollLeft
-            shape.y = ev.clientY + document.body.scrollTop
+            shape.x = ev.clientX //+ document.body.scrollLeft
+            shape.y = ev.clientY //+ document.body.scrollTop
         }
     }
 
-    var element = null
 
-    canvas.onclick = function (e) {
-        startDraw = !startDraw
-        if (element !== null) {
-            element = null
-            canvas.style.cursor = "default"
-        } else {
+    canvas.onmousedown = function (event) {
+        var e = event || window.event
+        if (e.target && e.target.id == shapeArea) {
+            index++
+            drawing = true
             shape.startX = shape.x
             shape.startY = shape.y
+            const attributeName = rectangle + index
             element = document.createElement('canvas')
-            element.className = 'rectangle'
+            element.className = attributeName
+            element.id = attributeName
             element.style.position = 'absolute'
-            element.style.left = shape.x + 'px'
-            element.style.top = shape.y + 'px'
+            element.style.left = shape.x + px
+            element.style.top = shape.y + px
+            element.style.width = '0px'
+            element.style.height = '0px'
+            element.style.border = '1px solid'
             element.style.backgroundColor = randomColor()
+            element.style.cursor = "pointer"
+            element.style.opacity = '1';
+            element.addEventListener("dblclick",
+                function (e) {
+                    const item = e.target
+                    const duration = '5s'
+                    const timeOut = '5000'
+                    tabItemToRotate.push(item.id)
+                    tabItemToRemove.push(item)
+                    const rotationAngle = 'rotate(360deg)'
+                    var promise = new Promise(resolve => setTimeout(function () {
+                        item.style.WebkitTransitionDuration = duration
+                        item.style.webkitTransform = rotationAngle
+                        tabItemToRotate.splice(tabItemToRotate.indexOf(item))
+                        resolve(true)
+                    }, timeOut))
+                    promise.then((value) => {
+                        if (tabItemToRotate.length == 0) {
+                            tabItemToRemove.forEach(function (rect) {
+                                rect.style.opacity = '0'
+                                setTimeout(function () {
+                                    rect.remove()
+                                }, timeOut)
+                            });
+                        }
+                    });
+
+                })
             canvas.appendChild(element)
             canvas.style.cursor = "crosshair"
+
         }
     }
 
     canvas.onmousemove = function (e) {
         setMousePosition(e)
-        if (element !== null) {
-            element.style.width = startDraw==true? Math.abs(shape.x - shape.startX) + 'px': '0px'
-            element.style.height = startDraw==true ? Math.abs(shape.y - shape.startY) + 'px': '0px'
-            element.style.left = (shape.x - shape.startX < 0) ? shape.x + 'px' : shape.startX + 'px'
-            element.style.top = (shape.y - shape.startY < 0) ? shape.y + 'px' : shape.startY + 'px'
+        if (drawing) {
+            element.style.width = Math.abs(shape.x - shape.startX) + px
+            element.style.height = Math.abs(shape.y - shape.startY) + px
+            element.style.left = (shape.x - shape.startX < 0) ? shape.x + px : shape.startX + px
+            element.style.top = (shape.y - shape.startY < 0) ? shape.y + px : shape.startY + px
+
         }
     }
+    canvas.onmouseup = function () {
+        drawing = false
 
-}
-
-function randomColor() {
-    const numb = '0123456789ABCDEF'
-    let color = '#'
-    for (let index = 0; index < 6; index++) {
-        color += numb[Math.floor(Math.random() * 16)]
     }
-    return color
+
 }
+
+
 
 
 initDraw()
--- public/js/util.ts
+++
@@ -1,8 +0,0 @@
-export function randomColor() {
-    const numb = '0123456789ABCDEF'
-    let color: String = '#'
-    for (let index = 0; index < 6; index++) {
-        color += numb[Math.floor(Math.random() * 16)]
-    }
-    return color
-}
